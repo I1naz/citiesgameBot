9a6bf447fa0374912ee40051956d14adc1f94409
I1naz <11ilnaz05@mail.ru>
2022-03-13 12:37:23 +0300

    first comit

---
+++ Markups.py
@@ -0,0 +1,12 @@
+from telebot import types
+
+
+markup_main = types.InlineKeyboardMarkup(row_width=3)
+btn_main = types.InlineKeyboardButton("Играть!", callback_data='play')
+markup_main.add(btn_main)
+
+markup_choice_first = types.InlineKeyboardMarkup(row_width=3)
+btn_choice_1 = types.InlineKeyboardButton('Я', callback_data='me')
+btn_choice_2 = types.InlineKeyboardButton('Ты', callback_data='you')
+markup_choice_first.add(btn_choice_1, btn_choice_2)
+
---
+++ pb.py
@@ -0,0 +1,58 @@
+import telebot
+from Config import *
+from Markups import *
+from Cities import *
+import random
+bot = telebot.TeleBot(Token)
+
+
+class Game:
+    def __init__(self):
+        self.count = 0
+        self.current_city = None
+        self.all_cities = None
+        self.first_letters = {'а': 0, 'б': 0, 'в': 0, 'г': 0, 'д': 0, 'е': 0, 'ё': 0, 'ж': 0, 'з': 0, 'и': 0, 'й': 0,
+                              'к': 0, 'л': 0, 'м': 0, 'н': 0, 'о': 0, 'п': 0, 'р': 0, 'с': 0, 'т': 0, 'у': 0, 'ф': 0,
+                              'х': 0, 'ц': 0, 'ч': 0, 'ш': 0, 'щ': 0, 'ю': 0, 'я': 0}
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, f'{start_mess1}{message.from_user.first_name}! {start_mess2}', parse_mode='html',
+                     reply_markup=markup_main)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, help_mess, parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+def play(message):
+    if message.chat.type == 'private':
+        # else:
+        #     bot.send_message(message.chat.id, ' А я чибурик!')
+        pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'you':
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Начинаю!',
+                                      reply_markup=None)
+            elif call.data == 'me':
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text='Начинай!', reply_markup=None)
+            elif call.data == 'play':
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Кто начинает?',
+                                 reply_markup=markup_choice_first)
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text='')
+    except Exception as e:
+        print(repr(e))
+
+
+if __name__ == '__main__':
+    game = Game()
+    bot.polling(none_stop=True)
